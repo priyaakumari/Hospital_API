b41f0969b08fd6dea30579f20e21bdedebf8d0e6
Priya Kumari <46952823+priyaakumari@users.noreply.github.com>
2023-06-20 20:42:34 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 
 //connect to DB
-mongoose.connect('mongodb://127.0.0.1:27017/hospital_API', { useNewUrlParser: true ,useUnifiedTopology: true });
+//mongoose.connect('mongodb://127.0.0.1:27017/hospital_API', { useNewUrlParser: true ,useUnifiedTopology: true });
+const DB = 'mongodb+srv://priyaprasad407:aFzmNLKvogdYOmOF@cluster0.venggr4.mongodb.net/hospital_API?retryWrites=true&w=majority'
+
+mongoose.connect(DB).then(()=>{
+     console.log('connection successful');
+ }).catch((err) => console.log("no connection " + err));
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to db"));
